08c70fd072c6dbd55e4aedb9129c5d606cde367b
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-09-06 14:12:48 +0200

    Next steps development & Details + coded Wikipedia request

--- french_republican_date.py
+++ french_republican_date.py
@@ -4,6 +4,8 @@
 # Author: Nicolas Flandrois
 
 import datetime
+from sys import exit
+import wikipedia
 
 
 class FrRepCal(object):
@@ -192,13 +194,10 @@ de l'An {date['FrRep_Year']} de la République"
 #     """Asking for Input in a specific Format,
 #     and convert it in a Specific Format accordingly,
 #     either French Republican Date Dictionary, or Gregorian Date Tuple."""
-#     def __init__(self, arg):
-#         super(Input, self).__init__()
-#         self.arg = arg
 
 
-# class Menu/APP(object):
-#     """Menu (or name it APP?)Will Manage the App"""
+class App(object):
+    """App will Manage the App"""
 # Menu Choices:
 # Print() > Disclaimer, See README on calculations, and considered approche
 # to dev. ROMME Computation(Or add this disclaimer in the menu)
@@ -222,3 +221,144 @@ de l'An {date['FrRep_Year']} de la République"
 #       wikipedia.page('French_Republican_calendar').url
 # 5/ Return to main menu
 # 6/ Quit
+
+
+    def clean():
+        """
+        This function will clear the terminal's screen. The command is
+        automaticaly detected according to the system OS you run it.
+        Compatible with Windows, OSx, and Linux.
+        """
+        os.system("cls" if platform.system() == "Windows" else "clear")
+
+
+    def ask_integer(message: str, range, error_message: str = ""):
+        """
+        This function's purpose is to ask and verify an Integer.
+        """
+        var = None
+        while True:
+            try:
+                var = int(input(message))
+                if var in range:
+                    return var
+                    raise
+
+            except KeyboardInterrupt:
+                break
+
+            except:
+                print(error_message)
+
+
+    def wikipedia(request: str):
+        """
+        Given a String, this function will fetch a wikipedia summary, and URL.
+        If not found, or exception raised, an error message will be displaid.
+        """
+        try:
+            return(wikipedia.summary(request), wikipedia.page(request).url)
+        except:
+            return ('Information not found.', 'Information not found.')
+
+
+    # @staticmethod
+    # def menu(question, choices=None):
+    #   """skeleton menu's view for each query and set of question."""
+    #   clean()
+    #   print(question)
+    #   if choices:
+    #       for num, choice in enumerate(choices):
+    #           print(str(num+1) + " : " + choice)
+    #   print('\n(Appuyer sur: Q pour QUITTER ou \
+    # R pour RETOUR au menu précédent.)\n')
+    #   while True:
+    #       try:
+    #           choice = input()
+    #           if choice.strip().lower() in ['r', 'q']:
+    #               break
+    #           elif choices:
+    #               choice = int(choice)
+    #               if choice in range(1, len(choices)+1):
+    #                   break
+    #               else:
+    #                   raise
+    #       except:
+    #           print(
+    #               "Veuillez entrer un nombre entre 1 et " +
+    #               str(len(choices)) + "."
+    #               )
+    #   return choice
+
+
+  # def main_menu():
+  #     """
+  #     Main Menu:
+  #       1/ Today's Fr Rep Date
+  #       2/ Convert a Greg Date into Fr Rep Date
+  #       3/ Translate a Fr Rep Date into Greg Date
+  #       4/ What is The French Republican Calendar
+  #       5/ Return to main menu
+  #       6/ Quit
+  #     """
+  # Using the (generic) Menu function
+
+
+  # def sub_menu():
+  #     """
+  #     Sub Menu:
+  #       a/ Long Format (Before 1802)
+  #       b/ Short Format (After 1802)
+  #       c/ Return to main menu
+  #       d/ Quit
+  #     """
+  # Using the (generic) Menu function
+
+
+  # def main():
+  #   """
+  #   This function will run the program according to user story, and menu logic.
+  #   """
+  # Exemple:
+  #       def set_view(view, top_level=False):
+  #           res = view()
+  #           if res == "r":
+  #               if not top_level:
+  #                   return None
+  #           elif res == "q":
+  #               exit()
+  #           else:
+  #               return res
+
+
+  #       def main():
+  #           """Program's Main running function."""
+
+  #           while True:
+  #               res = set_view(View.main_view, True)
+  #               if res == 1:
+  #                   while True:
+  #                       cat_id = set_view(View.cats_view)
+  #                       if cat_id:
+  #                           while True:
+  #                               prod_id = set_view(lambda: View.prods_view(cat_id))
+  #                               if not prod_id:
+  #                                   break
+  #                               while True:
+  #                                   set_view(lambda: View.sheet_view(prod_id))
+  #                                   set_view(lambda: View.substitution(prod_id))
+  #                                   break
+  #                       if not cat_id:
+  #                           break
+
+  #               elif res == 2:
+  #                   set_view(View.sub_tbl)
+  #                   res = set_view(lambda: View.menu("Question:", [
+  #                       "Retour Menu Principal", "Quitter"]))
+  #                   if res == 2:
+  #                       exit()
+  #                   if not res:
+  #                       continue
+
+# if __name__ == '__main__':
+#     App.main()
--- test.py
+++ test.py
@@ -182,3 +182,23 @@ def test_view_a1802():
     assert View.fr_date_a1802(Compute.convert(datetime.date(2019, 12, 31).
                                               timetuple())) == \
         "11 Nivôse, de l'An 228 de la République"
+
+def test_wikipedia():
+    assert type(App.wikipedia('London')) == tuple
+    assert App.wikipedia('french_republican_date')[0] == \
+        "The French Republican calendar (French: calendrier républicain \
+français), also commonly called the French Revolutionary calendar \
+(calendrier révolutionnaire français), was a calendar created and implemented \
+during the French Revolution, and used by the French government for about 12 \
+years from late 1793 to 1805, and for 18 days by the Paris Commune in 1871. \
+The revolutionary system was designed in part to remove all religious and \
+royalist influences from the calendar, and was part of a larger attempt at \
+decimalisation in France (which also included decimal time of day, \
+decimalisation of currency, and metrication). It was used in government \
+records in France and other areas under French rule, including Belgium, \
+Luxembourg, and parts of the Netherlands, Germany, Switzerland, Malta, and \
+Italy."
+    assert App.wikipedia('french_republican_date')[1] == \
+        "https://en.wikipedia.org/wiki/French_Republican_calendar"
+    assert App.wikipedia('zsecfu')[0] == 'Information not found.'
+    assert App.wikipedia('zsecfu')[1] == 'Information not found.'
